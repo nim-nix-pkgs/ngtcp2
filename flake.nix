cdf034ff927f888414ab148815d463bacb08cf6d
hugosenari <hugosenari@gmail.com>
2022-05-09 20:40:51 -0300

    chore: re index ngtcp2

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."ngtcp2-main".dir   = "main";
-  inputs."ngtcp2-main".owner = "nim-nix-pkgs";
-  inputs."ngtcp2-main".ref   = "master";
-  inputs."ngtcp2-main".repo  = "ngtcp2";
-  inputs."ngtcp2-main".type  = "github";
-  inputs."ngtcp2-main".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."ngtcp2-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."ngtcp2-0_32_0".dir   = "0_32_0";
-  inputs."ngtcp2-0_32_0".owner = "nim-nix-pkgs";
-  inputs."ngtcp2-0_32_0".ref   = "master";
-  inputs."ngtcp2-0_32_0".repo  = "ngtcp2";
-  inputs."ngtcp2-0_32_0".type  = "github";
-  inputs."ngtcp2-0_32_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."ngtcp2-0_32_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
